d569966be48e1637b500a844f42c28b59cbd1e9c
teklaaivazashvili <tekla.aivazashvili@gau.edu.ge>
2024-12-14 17:27:31 +0400

    sections 7, 8

---
+++ src/dev/omedia/ex-40/Mitsubishi.java
@@ -0,0 +1,20 @@
+public class Mitsubishi extends Car {
+    public Mitsubishi(int cylinders, String name) {
+        super(cylinders, name);
+    }
+
+    @Override
+    public String startEngine() {
+        return "Mitsubishi -> engine starting";
+    }
+
+    @Override
+    public String accelerate() {
+        return "Mitsubishi -> accelerating";
+    }
+
+    @Override
+    public String brake() {
+        return "Mitsubishi -> braking";
+    }
+}
